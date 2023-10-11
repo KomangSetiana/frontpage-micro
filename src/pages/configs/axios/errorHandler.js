af3c1856b8efb8ed93e6387cf498189a1872d68f
I Komang Setiana <komangsetiana40@gmail.com>
2023-10-11 16:46:25 +0800

    initial-commit

--- src/pages/configs/axios/errorHandler.js
+++ src/pages/configs/axios/errorHandler.js
@@ -1,18 +1,15 @@
-import {toast} from 'react-toastify'
-
-
+import { toast } from "react-toastify";
 
 export default function errorHandler(error) {
-    if(error) {
-  let message;
-  if(error.response) {
-    if(error.response.status === 500 ) message = "Something when wrong"
-    else message = error.message.data.message
+  if (error) {
+    let message;
+    if (error.response) {
+      if (error.response.status === 500) message = "Something when wrong";
+      else message = error.message.data.message;
 
-    if(typeof message === "string") toast.error(message)
+      if (typeof message === "string") toast.error(message);
 
-    return Promise.reject(error)
-
-  }
+      return Promise.reject(error);
     }
+  }
 }
--- src/pages/configs/axios/index.js
+++ src/pages/configs/axios/index.js
@@ -1,11 +1,11 @@
-import axios from 'axios'
+import axios from "axios";
 
-import errorHandler from './errorHandler'
+import errorHandler from "./errorHandler";
 
 const intance = axios.create({
-    baseURL : `${process.env.NEXT_PUBLIC_API_HOST}`
-})
+  baseURL: `${process.env.NEXT_PUBLIC_API_HOST}`,
+});
 
-intance.interceptors.response.use( (response) => response.data, errorHandler)
+intance.interceptors.response.use((response) => response.data, errorHandler);
 
-export default intance
+export default intance;
